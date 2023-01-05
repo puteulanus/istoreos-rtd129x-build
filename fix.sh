7d0420e4ea0bb3d7270473aa8a26927259d7be3f
Puteulanus <mj19941230@gmail.com>
2023-01-05 09:05:14 +0000

    init

---
+++ fix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -r include/soc/realtek istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/soc/
+cp include/dt-bindings/soc/rtd129x,memory.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/soc/
+cp include/dt-bindings/clock/rtk,clock-rtd129x.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/clock/
+cp include/dt-bindings/reset/rtk,reset.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/reset/
+cp include/dt-bindings/reset/rtk,reset-rtd129x.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/reset/
+cp include/dt-bindings/regulator/gmt,g22xx.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/regulator/
+cp include/dt-bindings/power/rtk,power-rtd129x.h istoreos/build_dir/target-aarch64_cortex-a53_musl/linux-realtek_rtd129x/linux-4.9.282/include/dt-bindings/power/
